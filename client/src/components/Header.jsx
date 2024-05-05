cef1a2b9247546fb02c00a33ebb298ba9fd5cf8a
Damien <damienambient@gmail.com>
2024-05-05 23:28:09 +0300

    added change theme light/dark

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
+    const { theme } = useSelector((state) => state.theme);
+
     return (
         <Navbar className="border-b-2">
             <Link
@@ -34,8 +38,9 @@ export default function Header() {
                     className=" w-12 h-10 hidden sm:inline"
                     color="gray"
                     pill
+                    onClick={() => dispatch(toggleTheme())}
                 >
-                    <FaMoon></FaMoon>
+                    {theme === "light" ? <FaSun></FaSun> : <FaMoon></FaMoon>}
                 </Button>
                 {currentUser ? (
                     <Dropdown
