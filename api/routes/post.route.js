e2a49ba6e04a0fc9418938cef717d96fea9a159f
Damien <damienambient@gmail.com>
2024-05-21 18:27:03 +0300

    delete post func from dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { create, getPosts } from "../controllers/post.controller.js";
+import {
+    create,
+    deletepost,
+    getPosts,
+} from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 
 export default router;
