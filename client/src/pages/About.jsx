ae2a3ab6cc79a0c0564f0ee6a0dfd305089e3ef4
Damien <damienambient@gmail.com>
2024-06-09 21:17:16 +0300

    completed about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,21 @@
-import React from "react";
-
 export default function About() {
-    return <div>About</div>;
+    return (
+        <div className=" min-h-screen flex items-center justify-center">
+            <div className=" max-w-2xl mx-auto p-3 text-center">
+                <div>
+                    <h1 className=" text-3xl font-semibold text-center my-7">
+                        About Damien's blog
+                    </h1>
+                    <div className="text-md text-gray-500 flex flex-col gap-6">
+                        <p>
+                            Welcome to Damien's blog! This blog is a test
+                            project created by Damien K.
+                        </p>
+                        <p>This website was built with MERN-stack</p>
+                        <p>Big thanks to Sahand G.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
